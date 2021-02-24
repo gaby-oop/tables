d5b43842a243021cd1edfe1c46b4e0f952ce4f1b
gaby-oop <gabysanz1505@gmail.com>
2021-02-24 18:06:16 +0000

    table

--- src/models.py
+++ src/models.py
@@ -7,27 +7,43 @@ from sqlalchemy import create_engine
 from eralchemy import render_er
 
 Base = declarative_base()
-
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class usuario(Base):
+    __tablename__ = 'usuario'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
+    name = Column(String(200), nullable=False)
+    password = Column(String(200))
+   
+class Personajes(Base):
+    __tablename__ = 'personajes'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(200))
+    estatura = Column(String(200))
+    birthyear= Column(String(200), nullable=False)
+    gender= Column(String(200))
 
+class Planetas(Base):
+    __tablename__ = 'planetas'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200))
+    size = Column(String(200))
+    diametro = Column(String(200), nullable=False)
+    gravedad = Column(String(200))
+
+class favoritos(Base):
+    __tablename__ = 'favoritos'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200))
+    personajes_id  = Column(Integer, ForeignKey ("personajes.id"))
+    planetas_id  = Column(Integer, ForeignKey ("planetas.id"))
+    usuario_id  = Column(Integer, ForeignKey ("usuario.id"))
     def to_dict(self):
         return {}
+render_er(Base, 'diagram.png')
 
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
